c21dbd1a9074a7a47cdf7874ed062d24799d220f
Vignesh <vignesh000129@gmail.com>
2023-08-09 23:25:39 +0530

    Generated component service and common

--- 02-frontend/katzMovieBooking/src/app/app.module.ts
+++ 02-frontend/katzMovieBooking/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovielistsComponent } from './compomnents/movielists/movielists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovielistsComponent
   ],
   imports: [
     BrowserModule
