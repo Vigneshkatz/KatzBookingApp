362fdda73728bfef3ad02e55ef590c3c8684fcf4
Vignesh <vignesh000129@gmail.com>
2023-08-09 22:07:21 +0530

    Springboot setup

---
+++ 01-backend/moviebooking/src/test/java/com/katziio/moviebooking/MoviebookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katziio.moviebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
