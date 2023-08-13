38643abafafefa3b669bf3b0666d2a32cb7387b1
Vignesh <107601806+Vigneshkatz@users.noreply.github.com>
2023-08-13 16:48:06 +0530

    Added pagination support

--- 01-backend/moviebooking/src/main/java/com/katziio/moviebooking/dao/MovieRestRepository.java
+++ 01-backend/moviebooking/src/main/java/com/katziio/moviebooking/dao/MovieRestRepository.java
@@ -4,6 +4,11 @@ import com.katziio.moviebooking.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface MovieRestRepository extends JpaRepository<Movie,Long> {
+@CrossOrigin("http://localhost:4200")
+//public interface MovieRestRepository extends JpaRepository<Movie,Long> {
+//
+//
+//}
 
+public interface MovieRestRepository extends PagingAndSortingRepository<Movie, Long> {
 }
